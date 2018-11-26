13138642adf74b01a3bb7c31ff13f576705ae139
spemer <ghsspower@naver.com>
2018-11-27 04:14:08 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import 'babel-polyfill'
+import 'es6-promise/auto'
+import 'event-source-polyfill'
+import 'promise-polyfill/src/polyfill'
+
+import Vue from 'vue'
+import axios from 'axios'
+import App from './App.vue'
+import router from './routes'
+import VTooltip from 'v-tooltip'
+import VueScrollTo from 'vue-scrollto'
+
+import {globalVar} from './globalVar'
+
+Vue.config.productionTip = false
+Vue.prototype.$http = axios
+
+Vue.use(VTooltip)
+Vue.use(VueScrollTo)
+Kakao.init(process.env.KAKAO_KEY)
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Meta from 'vue-meta'
+
+Vue.use(VueRouter)
+Vue.use(Meta)
+
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    // {
+    //   path: '*',
+    //   redirect: '/'
+    // },
+  ]
+})
