18164c3dbad69673a169cc9a90bc7cbaefb58426
spemer <ghsspower@naver.com>
2018-12-08 16:43:42 +0900

    ADD header component

---
+++ src/components/Header.vue
@@ -0,0 +1,99 @@
+<template lang="pug">
+  div#header
+    div.container
+      div.header__left(
+        v-scroll-to="{el: 'body', duration: 0}"
+        v-tooltip.bottom="{content: '맨 위로', delay: {show: 500, hide: 100}}"
+      )
+        img.header__left--img(
+          src="../assets/dist/launcher.svg"
+        )
+        p.header__left--text {{ serviceKo }} - {{ tagline }}
+
+      div.header__right(
+        v-scroll-to="{el: '#download', duration: 0, offset: -54}"
+        v-tooltip.bottom="{content: '어플 다운로드 받기', delay: {show: 500, hide: 100}}"
+      )
+        p.header__right--text 어플 다운로드
+</template>
+
+<script>
+import {globalVar} from '@/globalVar'
+
+export default {
+  name: 'header-el',
+
+  data () {
+    return {
+      tagline: globalVar.tagline,
+      serviceKo: globalVar.serviceKo,
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+
+#header {
+  top: 0;
+  width: 100%;
+  z-index: 10;
+  position: fixed;
+  height: $grid14x;
+  font-weight: 900;
+  background-color: #fff;
+  @include box-shadow();
+
+  // safari
+  @supports (-webkit-appearance:none) {
+    background-color: hsla(0, 0%, 100%, .8);
+    -webkit-backdrop-filter: saturate(180%) blur(16px);
+            backdrop-filter: saturate(180%) blur(16px);
+  }
+
+  // edge
+  @supports (-ms-ime-align:auto) {
+    background-color: hsla(0, 0%, 100%, .8);
+    -webkit-backdrop-filter: saturate(180%) blur(16px);
+            backdrop-filter: saturate(180%) blur(16px);
+  }
+
+  @media #{$ie}      { background-color: #fff; }
+  @media #{$chrome}  { background-color: #fff; }
+  @media #{$firefox} { background-color: #fff; }
+
+
+  .header__left {
+    cursor: pointer;
+    height: $grid14x;
+    display: inline-block;
+
+    .header__left--img {
+      padding: $grid2x 0;
+    }
+
+    .header__left--text {
+      color: $black78;
+      font-weight: 400;
+      margin-left: $grid4x;
+      display: inline-block;
+      @include transform(translateY(-75%));
+    }
+  }
+
+  .header__right {
+    float: right;
+    color: $brand-pink;
+    cursor: pointer;
+    transition: color .25s ease;
+
+    &:hover {
+      color: $brand-pink-pressed;
+    }
+
+    .header__right--text {
+      @include transform(translateY(-10%));
+    }
+  }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,17 @@
 <template lang="pug">
   div#home
+    Header
     Kakao
 </template>
 
 <script>
+import Header from '@/components/Header'
 import Kakao from '@/components/misc/Kakao'
 
 export default {
   name: 'home',
   components: {
+    Header,
     Kakao,
   }
 }
