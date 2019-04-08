b99233623be764157154610eba5b05b0c698bf40
spemer <ghsspower@gmail.com>
2019-04-09 00:16:19 +0900

    Add files via upload

--- src/globalVar.js
+++ src/globalVar.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 
 export const globalVar = new Vue({
   data: {
-    companyName: 'RsFive',
+    companyName: '알에스파이브',
     companyNameFull: '(주)알에스파이브',
+    companyNameFullKo: '주식회사 알에스파이브',
     companyAdr: '서울특별시 송파구 송파대로 345, 209동 904호(가락동, 헬리오시티)',
     companyCEO: '이문규',
     companyID: '653-12-01150',
@@ -14,8 +15,8 @@ export const globalVar = new Vue({
     tagline: '실시간 쇼핑 스트리밍',
     taglineLong: '라이브 방송으로 만나는 동대문 쇼핑',
 
-    callTo: '010-7422-8845',
-    mailTo: 'riversouthfive@gmail.com',
+    callTo: '+82-10-7422-8845',
+    mailTo: 'alex.lee@volla.live',
 
     websiteUrl: 'https://volla.live',
     ceoWebsiteUrl: 'https://volla.live/admin',
