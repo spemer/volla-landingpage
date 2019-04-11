cc065f9dbde6b1ea16f89955c76bdf03870d2c39
spemer <ghsspower@gmail.com>
2019-04-12 03:57:59 +0900

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
   div#app
     router-view
-    vue-progress-bar
+    //- vue-progress-bar
 </template>
 
 <script>
 import Vue from 'vue'
-import VueProgressBar from 'vue-progressbar'
+// import VueProgressBar from 'vue-progressbar'
 import {globalVar} from '@/globalVar'
 
-Vue.use(VueProgressBar, {
-  color: '#ff82ab',
-  height: '2px',
-})
+// Vue.use(VueProgressBar, {
+//   color: '#ff82ab',
+//   height: '2px',
+// })
 
 export default {
   name: 'app',
@@ -22,23 +22,23 @@ export default {
     titleTemplate: '%s' + ' - ' + globalVar.tagline,
   },
 
-  mounted () {
-    this.$Progress.finish()
-  },
+  // mounted () {
+  //   this.$Progress.finish()
+  // },
 
-  created () {
-    this.$Progress.start()
-    this.$router.beforeEach((to, from, next) => {
-      if (to.meta.progress !== undefined) {
-        let meta = to.meta.progress
-        this.$Progress.parseMeta(meta)
-      }
-      this.$Progress.start()
-      next()
-    })
-    this.$router.afterEach((to, from) => {
-      this.$Progress.finish()
-    })
-  },
+  // created () {
+  //   this.$Progress.start()
+  //   this.$router.beforeEach((to, from, next) => {
+  //     if (to.meta.progress !== undefined) {
+  //       let meta = to.meta.progress
+  //       this.$Progress.parseMeta(meta)
+  //     }
+  //     this.$Progress.start()
+  //     next()
+  //   })
+  //   this.$router.afterEach((to, from) => {
+  //     this.$Progress.finish()
+  //   })
+  // },
 }
 </script>
