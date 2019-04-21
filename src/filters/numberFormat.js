6aca21556c9a17b7c7dda575b074b8f613c486ba
spemer <ghsspower@gmail.com>
2019-04-22 03:01:31 +0900

    Add files via upload

--- src/filters/numberFormat.js
+++
@@ -1,8 +0,0 @@
-// export default (value = '') => {
-//   value = '' + value
-
-//   return value.split('').reverse().reduce((acc, digit, i) => {
-//     if (i > 0 && i % 3 === 0) acc.push(',')
-//     return [...acc, digit]
-//   }, []).reverse().join('')
-// }
