94f0a0ace0a1cab0538bb4472a19a7f9a9dfa5fb
Hyouk Seo <ghsspower@naver.com>
2019-10-17 18:13:41 +0900

    가이드 순서 변경

--- src/pages/guide/Guide-Prism.vue
+++ src/pages/guide/Guide-Prism.vue
@@ -2,7 +2,7 @@
   div#guide-prism
     div.guide-live_title 프리즘(송출 앱) 설정하기
     div.guide-prism_imgbox(
-      v-for="n in 4"
+      v-for="n in 5"
     )
       img.guide-prism_img(
         :src="require(`@/assets/guide/dist/prism-${n}.png`)"
