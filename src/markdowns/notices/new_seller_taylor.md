d1f8ab51a05df485c9e8b353868557c2a575fb22
Hyouk Seo <ghsspower@naver.com>
2019-10-24 17:37:07 +0900

    셀러 추가, 가이드 수정, 에셋 최신화

---
+++ src/markdowns/notices/new_seller_taylor.md
@@ -0,0 +1,15 @@
+# [입점] Taylor_crush_Acc
+
+![Taylor_crush_Acc](../../assets/marketing/dist/seller-taylor.png)
+
+그대들의 데일리를 책임질 쥬얼리샵, #테일러크러쉬 님이 볼라에 입점하셨어요.
+
+'모던심플우아' 스타일의 악세서리부터, '키치블링러블리' 스타일의 주얼리까지!
+
+#taylor_crush_acc 님의 개성넘치는 악세서리들을 볼라 앱에서 만나보세요.
+
+- [Taylor_crush_Acc 님 셀러 페이지 바로가기](volla://deeplink/seller/31)
+
+<br>
+
+라이브 방송으로 만나는 오프라인 쇼핑, Volla
--- src/pages/sellerform/SellerForm.vue
+++ src/pages/sellerform/SellerForm.vue
@@ -89,9 +89,11 @@
           span.checkmark
 
         p.sellerform-guideline
-          span 볼라는 10~30대 초반 여성을 대상으로한 여성 쇼핑몰로,
+          span * 볼라는 10~30대 초반 여성을 대상으로한 쇼핑몰로,
           br
-          span 해당 기준에서 벗어난다고 판단되거나 거래처 사진을 사용하는 샵의 경우 입점이 제한될 수 있습니다.
+          span 해당 기준에서 벗어난다고 판단되거나 가품 판매 및 거래처
+          br
+          span 사진을 사용하는 샵의 경우 입점이 제한될 수 있습니다.
 
         div.sellerform-wrapper(
           :class="{'apply_border': this.$route.query.from === 'app'}"
@@ -308,7 +310,7 @@ $mobile-width: 288px;
     color: $black38;
     display: inline-block;
     margin-top: -#{$grid4x};
-    margin-bottom: $grid16x;
+    margin-bottom: $grid20x;
     line-height: $grid4x;
 
     span {
