393cd57b50e27507a0a9c99013a96ea97c714d8a
Hyouk Seo <ghsspower@naver.com>
2019-12-02 15:41:00 +0900

    문구 변경[C

--- src/pages/sellerform/SellerForm.vue
+++ src/pages/sellerform/SellerForm.vue
@@ -91,9 +91,9 @@
         p.sellerform-guideline
           span * 볼라는 10~30대 초반 여성을 대상으로한 쇼핑몰로,
           br
-          span 해당 기준에서 벗어난다고 판단되거나 가품 판매 및 거래처
+          span 해당 기준에서 벗어난다고 판단되거나, 볼라에서 가품 및
           br
-          span 사진을 사용하는 샵의 경우 입점이 제한될 수 있습니다.
+          span 레플리카를 판매하는 샵의 경우 입점이 제한될 수 있습니다.
 
         div.sellerform-wrapper(
           :class="{'apply_border': this.$route.query.from === 'app'}"
